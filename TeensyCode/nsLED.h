15d20938aefd8159e933ab56565546c4ea22c368
unSilentArt <68631863+unSilentArt@users.noreply.github.com>
2020-07-22 16:25:38 +0800

    Rename nsLED.h to TeensyCode/nsLED.h

---
+++ TeensyCode/nsLED.h
@@ -0,0 +1,22 @@
+#ifndef __NS_LED__
+#define __NS_LED__
+
+class nsLED
+{
+    public:
+        nsLED();
+
+        void    init(void);
+        void    update(void);
+        void    setBrightness(int led, int bright);
+        int     getBrightness(int led)  {return mBrightness[led];}
+
+    private:
+        unsigned long   mLastUpdate;
+        int             mCount;
+
+        int             mBrightness[8];
+};
+
+
+#endif  //  __NS_LED__
