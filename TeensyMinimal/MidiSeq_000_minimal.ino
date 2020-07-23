da08804abd334f769438e51c4ddfefc82f465910
unSilentArt <68631863+unSilentArt@users.noreply.github.com>
2020-07-23 12:15:26 +0800

    Add files via upload

---
+++ TeensyMinimal/MidiSeq_000_minimal.ino
@@ -0,0 +1,197 @@
+
+#include "nsNoteManager.h"
+#include "nsArp.h"
+
+const int ledPin = 13;
+int ledState;
+int loopCounter;
+
+uint8_t draw_state = 0;
+int 	ledCounter;
+
+NsArp       theArp;
+NoteQueue 	noteQueue;
+
+unsigned long songPosition;
+unsigned long lastDisplayUpdate;
+unsigned long ledTimer;
+
+
+
+//----------------------------------------------------------------------------------------------------------
+void setup()
+  {
+  songPosition = 0;
+  ledState=0;
+  ledCounter=0;
+  ledState=0;
+  ledTimer=0;
+  loopCounter=0;
+
+
+  pinMode(ledPin, OUTPUT);
+
+  usbMIDI.setHandleNoteOff(OnNoteOff);
+  usbMIDI.setHandleNoteOn(OnNoteOn);
+  usbMIDI.setHandleVelocityChange(OnVelocityChange);
+  usbMIDI.setHandleControlChange(OnControlChange);
+  usbMIDI.setHandleProgramChange(OnProgramChange);
+  usbMIDI.setHandleAfterTouch(OnAfterTouch);
+  usbMIDI.setHandlePitchChange(OnPitchChange);
+
+
+  usbMIDI.setHandleTimeCodeQuarterFrame(onTimeCodeQuarterFrame);
+  usbMIDI.setHandleSongPosition(onHandleSongPosition);
+  usbMIDI.setHandleClock(onHandleClock);
+  usbMIDI.setHandleStart(onStart);
+  usbMIDI.setHandleContinue(onHandleContinue);
+  usbMIDI.setHandleStop(onHandleStop);
+  usbMIDI.setHandleActiveSensing(onHandleActiveSensing);
+  usbMIDI.setHandleSystemReset(onHandleSystemReset);
+  usbMIDI.setHandleRealTimeSystem(onHandleRealTimeSystem);
+  }
+
+// TEENSY LED______________________________________________________________________________
+void doLed()
+{
+  unsigned long timeNow = millis();
+  if(timeNow > ledTimer)
+  {
+    ledTimer += 250;
+    ledCounter++;
+    if(ledCounter>=40)
+    {
+      ledCounter=0;
+      digitalWrite(ledPin, LOW);   // set the LED off
+      ledState = LOW;
+    }
+      
+    if((ledCounter>=30)&&(ledState==LOW))
+    {
+      ledState = HIGH;
+      digitalWrite(ledPin, HIGH);   // set the LED on
+    }
+  }
+}
+
+void  panicReset()
+{
+    songPosition = 0;
+    ledState=0;
+    ledCounter=0;
+    ledState=0;
+    ledTimer=0;
+    loopCounter=0;
+
+    noteQueue.reset();
+    theArp.reset();
+
+    delay(500);
+
+    int nNum;
+  
+    for(int n=0; n<13; n++)
+    {
+      nNum=60;
+
+      if(n%3==1)
+        nNum=64;
+        
+      if(n%3==2)
+        nNum=67;
+        
+      noteQueue.addNote(nNum, 90, 1, 24+1+n*24);
+      noteQueue.addNote(nNum,  0, 1, 24+1+(n+1)*24);
+    }
+  }
+
+//________________________________________________________________________________________________________________________________
+//
+//	MIDI event processing
+//________________________________________________________________________________________________________________________________
+void loop()
+  {
+  noteQueue.processQueue(usbMIDI,songPosition);
+  usbMIDI.read();
+  }
+
+//________________________________________________________________________________________________________________________________
+void OnNoteOn(byte channel, byte note, byte velocity) 
+  {
+  theArp.midiNoteOnReceived(note, velocity, channel,songPosition);
+  }
+
+//________________________________________________________________________________________________________________________________
+void OnNoteOff(byte channel, byte note, byte velocity) 
+  {
+  theArp.midiNoteOffReceived(note, velocity, channel,songPosition);
+  }
+
+//________________________________________________________________________________________________________________________________
+void OnVelocityChange(byte channel, byte note, byte velocity) 
+{
+}
+
+void OnControlChange(byte channel, byte control, byte value) 
+{
+}
+
+void OnProgramChange(byte channel, byte program) 
+{
+}
+
+void OnAfterTouch(byte channel, byte pressure)
+{
+}
+
+void OnPitchChange(byte channel, int pitch)
+{
+}
+
+void onTimeCodeQuarterFrame(uint8_t data)
+{
+}
+void onHandleSongPosition(uint16_t beats)
+{
+  songPosition = beats*6;
+
+}
+void onHandleClock(void)
+{
+  theArp.midiClockReceived(songPosition, &noteQueue);
+  songPosition++;
+
+/*
+  int beatNum = songPosition / 24;
+  int barNum = 1 + int(songPosition) / 96;
+  int barBeat = 1 + beatNum%4;
+*/
+}
+
+void onStart(void)
+{
+  songPosition = 0;
+}
+
+void onHandleContinue(void)
+{
+}
+
+void onHandleStop(void)
+{
+  noteQueue.midiStop(usbMIDI);
+  theArp.midiStopReceived();
+}
+
+void onHandleActiveSensing(void)
+{
+}
+
+void onHandleSystemReset(void)
+{
+  onHandleStop();
+}
+
+void onHandleRealTimeSystem(uint8_t realtimebyte)
+{
+}
---
+++ TeensyMinimal/nsArp.cpp
@@ -0,0 +1,109 @@
+// NsArp.cpp
+#include "nsArp.h"
+
+#include "nsNoteManager.h"
+
+#include <Arduino.h>
+#include "usb_midi.h"
+
+
+//-------------------------------------------------------------------------------------
+NsArp::NsArp()
+{
+    mMidiChannel = 1;
+    mNumKeysCurrentlyDown=0;
+    mLastNotePlayedIdx=-1;
+
+    for(int i=0; i<mKeyListSize; i++)    
+    {
+        mKeyVelocity[i]=0;
+        mKeyTime[i]=0;
+    }
+}
+
+//-------------------------------------------------------------------------------------
+void NsArp::reset(void)
+{
+    mNumKeysCurrentlyDown=0;
+    mLastNotePlayedIdx=-1;
+
+    for(int i=0; i<mKeyListSize; i++)    
+    {
+        mKeyVelocity[i]=0;
+        mKeyTime[i]=0;
+    }
+
+}
+
+//-------------------------------------------------------------------------------------
+void NsArp::playNextNote(unsigned long songPosition,NoteQueue* pQueue)
+  {
+  int noteToPlay=-1;
+  int indexToPlay=-1;
+  int numFound=0;
+
+  indexToPlay = random(0, mNumKeysCurrentlyDown);
+
+  if(songPosition%96==0)
+    indexToPlay=0;
+
+  for(int i=0; i<mKeyListSize; i++)    
+    {
+      if(mKeyVelocity[i]>0)
+      {
+        if( numFound == indexToPlay )
+        {
+          noteToPlay = i;
+          indexToPlay = numFound;
+          break;
+        }
+
+      numFound++;
+      }
+    }
+
+  
+  if( noteToPlay>0)
+  {
+    pQueue->addNote(noteToPlay, 90, 1);                   // noteQueue - add note on
+    pQueue->addNote(noteToPlay, 0, 1,songPosition+6);     // noteQueue - add note off
+  }
+
+
+  mLastNotePlayedIdx = indexToPlay;
+  }
+
+//-------------------------------------------------------------------------------------
+void NsArp::midiNoteOnReceived(int note, int vel, int channel,unsigned long songPosition)
+{
+  if(mKeyVelocity[note]==0)
+    mNumKeysCurrentlyDown++;
+    
+  mKeyVelocity[note]=vel;
+  mKeyTime[note]=songPosition;
+}
+
+//-------------------------------------------------------------------------------------
+void NsArp::midiStopReceived(void)
+{
+    //reset();
+}
+
+//-------------------------------------------------------------------------------------
+void NsArp::midiNoteOffReceived(int note, int vel, int channel,unsigned long songPosition)
+{
+  if(mKeyVelocity[note]!=0)
+    mNumKeysCurrentlyDown--;
+ 
+  mKeyVelocity[note]=0;
+  mKeyTime[note]=0;
+}
+
+//-------------------------------------------------------------------------------------
+void NsArp::midiClockReceived(unsigned long songPosition,NoteQueue* pQueue)
+{
+    if(songPosition%12==0)
+    {
+      playNextNote(songPosition,pQueue);
+    }
+}
